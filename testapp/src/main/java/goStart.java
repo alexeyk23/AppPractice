7d8f0f23f853f2344bfb8ae4f51ae48aa34d296a
alexeyk <kunak94@mail.ru>
2014-07-13 19:30:27 +0400

    redacted persistens.xml and TRY to connect in java/goStart.java

--- testapp/src/main/java/goStart.java
+++ testapp/src/main/java/goStart.java
@@ -1,9 +1,34 @@
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+import javax.persistence.spi.PersistenceProvider;
+
+import org.hibernate.Hibernate;
+import org.hibernate.ejb.HibernatePersistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import com.relex.practice.pojo.Company;
+
+
+
+
 public class goStart {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	         //  System.out.println(System.getProperty("java.class.path").replace(';', '\n'));
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PTTest");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Company comp = new Company();
+		comp.setName("PIROG");
+		entityManager.persist(comp);
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 
 }
