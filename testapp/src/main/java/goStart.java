90f9ea0a61d77623e5c5ac5e1fe4b2a79e1d109c
alexeyk <kunak94@mail.ru>
2014-07-14 16:19:18 +0400

    Adeq COMMIT

--- testapp/src/main/java/goStart.java
+++ testapp/src/main/java/goStart.java
@@ -4,14 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
-import javax.persistence.PersistenceContext;
-import javax.persistence.spi.PersistenceProvider;
-
-import org.hibernate.Hibernate;
-import org.hibernate.ejb.HibernatePersistence;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-
 import com.relex.practice.pojo.Company;
 
 
@@ -20,7 +12,7 @@ import com.relex.practice.pojo.Company;
 public class goStart {
 
 	public static void main(String[] args) {
-	         //  System.out.println(System.getProperty("java.class.path").replace(';', '\n'));
+	   //      System.out.println(System.getProperty("java.class.path").replace(';', '\n'));
 
 		EntityManager entityManager;
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PTTest");
